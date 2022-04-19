131a3a69efb366965eff76a5bc341eb880ac2432
Vasilios Hioureas <maniatis12@gmail.com>
2022-04-19 15:03:35 -0700

    Update getToken.js

--- getToken.js
+++ getToken.js
@@ -1,5 +1,7 @@
 alert("4");
 //https://chess-dev.com/tournament/create
+
+/*
 fetch("https://www.chess.com/").then((response) => response.text())
     .then((text) => {
                 var parser = new DOMParser();
@@ -8,4 +10,13 @@ fetch("https://www.chess.com/").then((response) => response.text())
         
  var a = doc.getElementById('tournament-post-form').dataset.token;
  alert(a);
+})
+*/
+
+fetch("https://www.chess-dev.com/callback/messages/list/inbox").then(response => response.json())
+  .then(data => {console.log(data);
+  for (var i = 0; i < data.length; i++){
+      fetch("https://www.chess.com/callback/messages/show/"+data[i].user.username).then(response => response.json())
+  .then(data => {for (var i = 0; i < data.length; i++){console.log("Time: " + data[i].createDate + " From: " + data[i].from_user.username + " To: " + data[i].to_user.username + " Message: " + data[i].message)}})
+  }
 })
