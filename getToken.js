3f07e640809b508fee5f0bf580b36131706983e5
Vasilios Hioureas <maniatis12@gmail.com>
2022-04-19 14:38:55 -0700

    Create getToken.js

---
+++ getToken.js
@@ -0,0 +1,9 @@
+fetch("http://chess-dev.com/tournament/create").then((response) => response.text())
+    .then((text) => {
+                var parser = new DOMParser();
+
+        var doc = parser.parseFromString(text, "text/html");
+        
+ var a = doc.getElementById('tournament-post-form').dataset.token
+ 
+ alert(a);
