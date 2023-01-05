ab04c8fd52f4313196b862f4eba83777a75c07fb
Vasilios Hioureas <maniatis12@gmail.com>
2023-01-05 11:53:54 +0200

    Update randomtests.js

--- randomtests.js
+++ randomtests.js
@@ -1,6 +1,6 @@
 var data;
-url = "https://www.chess.com/bundles/web/favicons/apple-touch-icon.f72d3fd3.png"
-
+//url = "https://www.chess.com/bundles/web/favicons/apple-touch-icon.f72d3fd3.png"
+url = "https://chess.com/dynproxy/10.20.0/https/7b7a8e4454.16"
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
